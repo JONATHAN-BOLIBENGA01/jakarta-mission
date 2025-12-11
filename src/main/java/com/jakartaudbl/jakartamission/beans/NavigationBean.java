a6209ca65e12324a6a23d75bdccd1aafdc8ca97d
jonathan bolibenga <22bw023@esisalama.org>
2025-12-11 11:07:05 +0200

    creation des interffaces home, apropos et lieu

---
+++ src/main/java/com/jakartaudbl/jakartamission/beans/NavigationBean.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jakartaudbl.jakartamission.beans;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import java.io.IOException;
+
+/**
+ *
+ * @author jojo
+ */
+@Named(value="navigationController")
+@RequestScoped
+public class NavigationBean {
+   public void voirApropos(){
+       try{
+           FacesContext.getCurrentInstance().getExternalContext()
+                   .redirect("pages/a_propos.xhtml");
+       }catch(IOException e){
+           
+           e.printStackTrace();
+       }
+   }
+   
+   public void voirLieu(){
+       try{
+           FacesContext.getCurrentInstance().getExternalContext()
+                   .redirect("pages/lieux.xhtml");
+       }catch(IOException e){
+           
+           e.printStackTrace();
+       }
+   } 
+}
